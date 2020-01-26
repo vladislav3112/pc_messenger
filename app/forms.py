027e55cb33f1c8b6a1d9fb7feec35195247c95c9
dyushakolesov <dyushakolesov@mail.ru>
2020-01-26 16:52:13 +0300

    registeation form added

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
 
+class RegestrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('RepeatPassword', validators=[DataRequired(),EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('User alreaady exists.')
+    
+    def validate_email(self,email):
+        user = User.query.filter_by(username=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a diffirent email.')
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
