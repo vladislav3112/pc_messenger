696b4b05f4b20a32056d7210a2cfc649600643f9
dyushakolesov <dyushakolesov@mail.ru>
2020-01-27 20:45:56 +0300

    deployment on heroku(test)

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
